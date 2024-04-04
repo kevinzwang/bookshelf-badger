4d365d275754badeaf82ee7eb9bd9f716c8e1d9a
Stephanie Han <step.han.ie@berkeley.edu>
2024-04-04 13:28:10 -0700

    walking berry (no books)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,14 @@ public class PlayerController : MonoBehaviour
     float xAxis;
     float yAxis;
 
+    Animator anim;
+
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
         gameManager = GameManager.Instance;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,19 +36,30 @@ public class PlayerController : MonoBehaviour
 
     void movePlayer()
     {
+        anim.SetBool("Walking", true);
         Vector2 movementVector = new Vector2(xAxis, yAxis);
+        Vector2 currDir = movementVector.normalized;
+        
+        if (xAxis == 0 && yAxis == 0) {
+            anim.SetBool("Walking", false);
+        } else {
+            anim.SetFloat("DirX", currDir.x);
+            anim.SetFloat("DirY", currDir.y);
+        }
         playerRigidbody.velocity = movementVector.normalized * playerSpeed;
     }
 
 
     public void HoldBook(GameManager.Book book)
     {
-        playerSprite.color = book.color;
+        anim.SetBool("hasBook", true);
+        // playerSprite.color = book.color;
     }
 
     public void DropBook()
     {
-        playerSprite.color = new Color(1, 0.755348f, 0.495283f);
+        anim.SetBool("hasBook", false);
+        // playerSprite.color = new Color(1, 0.755348f, 0.495283f);
     }
 
     void OnMouseDown()
